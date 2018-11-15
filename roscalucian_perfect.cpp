615c9e021bab0e5b732f4932f0d2f4477203838f
Lucianandrei333 <44975419+Lucianandrei333@users.noreply.github.com>
2018-11-15 06:54:13 -0800

    Add files via upload

---
+++ roscalucian_perfect.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int n;
+int divizor;
+int putere;
+int
+int main()
+{
+    cout<<"N="; cin <<n;
+    while(n%divizor==0) { n=n/divizor;
+    putere=putere+1;}
+    if(n<){cout<<"nr este supraaglomerat";}
+    else{cout<< "nr nu este supraaglomerat";}
+    return 0;
+}
